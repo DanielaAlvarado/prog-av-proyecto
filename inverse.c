fee8796df7b8ad5bef8036a78ae9a461b497f1bb
Daniela Alvarado Pereda <danielaalvaradopereda@hotmail.com>
2019-04-29 12:40:36 -0500

    openmp

--- inverse.c
+++ inverse.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 
 float cofactor(float**, int, int, int);
-
-void printMatrix(float **matrix, int n){
-	for(int j = 0; j < n; j++){
-		for(int i = 0; i < n; i++){
-			printf("\t%f", matrix[j][i]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
 
 float determinant(float **matrix, int n){
     if(n == 1){
@@ -20,18 +11,35 @@ float determinant(float **matrix, int n){
     }
 
     float det = 0;
-    for(int j = 0; j < n; j++){
-        det += matrix[0][j] * cofactor(matrix, n, 0, j);
-    }
+	// #pragma omp parallel for reduction(+:det)
+	for(int j = 0; j < n; j++){
+		det += matrix[0][j] * cofactor(matrix, n, 0, j);
+	}
 
     return det;
 }
 
-float cofactor(float **matrix, int n, int row, int col){
-    float **minor = (float **) calloc(n - 1, sizeof(float *));
-    for(int i = 0; i < n - 1; i++){
-        minor[i] = (float *) calloc(n - 1 , sizeof(float));
+float **createMatrix(int n){
+	float **matrix = (float **) calloc(n, sizeof(float *));
+	// #pragma omp parallel for
+    for(int i = 0; i < n; i++){
+        matrix[i] = (float *) calloc(n, sizeof(float));
     }
+
+	return matrix;
+}
+
+void destroyMatrix(float **matrix, int n){
+	// #pragma omp parallel for
+    for(int i = 0; i < n; i++){
+        free(matrix[i]);
+    }
+
+	free(matrix);
+}
+
+float cofactor(float **matrix, int n, int row, int col){
+    float **minor = createMatrix(n - 1);
 
     int mi = 0;
     for(int i = 0; i < n; i++){
@@ -49,16 +57,14 @@ float cofactor(float **matrix, int n, int row, int col){
     }
 
     float cof = pow(-1, col + row + 2) * determinant(minor, n - 1);
-    free(minor);
+    destroyMatrix(minor, n - 1);
     return cof;
 }
 
 float **adjugate(float **matrix, int n){
-    float **adj = (float **) calloc(n, sizeof(float *));
-    for(int i = 0; i < n; i++){
-        adj[i] = (float *) calloc(n, sizeof(float));
-    }
+    float **adj = createMatrix(n);
 
+	// #pragma omp parallel for collapse(2)
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             adj[i][j] = cofactor(matrix, n, j, i);
@@ -72,36 +78,68 @@ float **inverse(float **matrix, int n){
     float **adj = adjugate(matrix, n);
     float det = determinant(matrix, n);
 
-    float **inv = (float **) calloc(n, sizeof(float *));
-    for(int i = 0; i < n; i++){
-        inv[i] = (float *) calloc(n, sizeof(float));
-    }
+    float **inv = createMatrix(n);
 
+	// #pragma omp parallel for collapse(2)
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             inv[i][j] = adj[i][j] / det;
         }
     }
 
-    free(adj);
+    destroyMatrix(adj, n);
     return inv;
 }
 
+
+void printMatrix(float **matrix, int n){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			printf("\t%f", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+
 int main(){
-    float **matrix = (float **) calloc(3, sizeof(float *));
-    for(int i = 0; i < 3; i++){
-        matrix[i] = (float *) calloc(3 , sizeof(float));
-    }
+	double start_time = omp_get_wtime();
 
-    matrix[0][0] = 2;
-    matrix[0][1] = 4;
-    matrix[0][2] = 3;
-    matrix[1][0] = 0;
-    matrix[1][1] = 1;
-    matrix[1][2] = -1;
-    matrix[2][0] = 3;
-    matrix[2][1] = 5;
-    matrix[2][2] = 7;
+	int n = 5;
+	float **matrix = createMatrix(n);
 
-    printMatrix(inverse(matrix, 3), 3);
+    matrix[0][0] = 5;
+    matrix[0][1] = 9;
+    matrix[0][2] = 8;
+    matrix[0][3] = 8;
+    matrix[0][4] = 5;
+	matrix[1][0] = 0;
+    matrix[1][1] = 7;
+    matrix[1][2] = 0;
+    matrix[1][3] = 9;
+    matrix[1][4] = 3;
+	matrix[2][0] = 4;
+    matrix[2][1] = 7;
+    matrix[2][2] = 9;
+    matrix[2][3] = 0;
+    matrix[2][4] = 0;
+	matrix[3][0] = 9;
+    matrix[3][1] = 1;
+    matrix[3][2] = 6;
+    matrix[3][3] = 1;
+    matrix[3][4] = 8;
+	matrix[4][0] = 4;
+    matrix[4][1] = 7;
+    matrix[4][2] = 6;
+    matrix[4][3] = 6;
+    matrix[4][4] = 5;
+
+	float **inv = inverse(matrix, n);
+	printMatrix(inv, n);
+	destroyMatrix(matrix, n);
+	destroyMatrix(inv, n);
+
+	double time = omp_get_wtime() - start_time;
+	printf("%f\n", time);
 }
